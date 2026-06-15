14e47c711cef56e268aa1dd77e757a81a74a9f23
jeflijonathan <jeflijonataha1@gmail.com>
2026-06-15 18:11:44 +0700

    Add email configuration to Env class and example.env

--- config/env/env.py
+++ config/env/env.py
@@ -18,5 +18,12 @@ class Env:
 
         self.dbLogging = os.getenv("DB_LOGGING", "false").lower() == "true"
 
+        self.mailUsername = os.getenv("MAIL_USERNAME", "your_email@gmail.com")
+        self.mailPassword = os.getenv("MAIL_PASSWORD", "your_app_password")
+        self.mailFrom = os.getenv("MAIL_FROM", "noreply@yourdomain.com")
+        self.mailPort = int(os.getenv("MAIL_PORT", 1025))
+        self.mailServer = os.getenv("MAIL_SERVER", "localhost")
+        self.mailFromName = os.getenv("MAIL_FROM_NAME", "Anti-Spoofing Admin")
+
 
 env = Env()
